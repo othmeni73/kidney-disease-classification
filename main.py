32a4b1552f7d8126e586c28e411e41ed18e54cf6
othmeni73 <63969735+othmeni73@users.noreply.github.com>
2025-01-20 22:15:13 +0100

    prepare vgg16 model

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
-from cnnDiseaseClassier import logger
-from cnnDiseaseClassier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from cnnDiseaseClassifier import logger
+from cnnDiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnDiseaseClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelPipeline
 STAGE_NAME = "Data Ingestion stage"
 
 try:
     logger.info(f">>>>>>>> stage {STAGE_NAME} started  <<<<<<<<<")
-    obj = DataIngestionTrainingPipeline()
-    obj.main()
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
     logger.info(f">>>>>>>> stage {STAGE_NAME} completed  <<<<<<<<<")
     
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME="Prepare base  model stage"
+
+try:
+    logger.info(f">>>>>>>> stage {STAGE_NAME} started  <<<<<<<<<")
+    prepare_base_model = PrepareBaseModelPipeline()
+    prepare_base_model.main()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} completed  <<<<<<<<<")
+       
 except Exception as e:
     logger.exception(e)
     raise e
